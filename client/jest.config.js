77594eba513def09feca7957888e9b3e373847b5
Goutham <dev.gouthamram95@gmail.com>
2021-01-04 14:08:02 +0530

    minor change to jest.config

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
 	testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
 	setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+	moduleDirectories: ['node_modules', 'src'],
 	transform: {
 		'^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
 	},
